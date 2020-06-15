3303de49b51321e160e21de2302cdb97163caedd
colevandersWands <18554853+colevandersWands@users.noreply.github.com>
2020-06-15 13:12:17 +0200

    comments

--- scripts/register.js
+++ scripts/register.js
@@ -1,11 +1,14 @@
+// require native modules
 const fs = require("fs");
 const path = require("path");
 const config = require("../config.json");
 
+// declare constants
 const EXERCISES_DIR = path.normalize(path.join(__dirname, '..', config.path));
 const EXAMPLE_WORDS = ['example', 'examples', 'worked', 'stepped', 'demo'];
 const PARENT_DIR = path.normalize(path.join(__dirname, '..'));
 
+// make sure the exercises directory exists
 try {
   fs.accessSync(EXERCISES_DIR);
 } catch (err) {
@@ -13,66 +16,90 @@ try {
   fs.mkdirSync(EXERCISES_DIR);
 };
 
+// a function for prettier logs, not very important
 const removeDirname = dirName => dirName.split(PARENT_DIR).join('  ...  ');
 
+// the function that will create an object with the same file paths as your /exercises folder
 const register = function (dirPath) {
   const dirs = [];
   const files = [];
 
+  // get an array of all file names in the directory
   const paths = fs.readdirSync(dirPath);
+
   for (let nextPath of paths) {
+    // if the file/folder name contains an example word, label it as such
     const isExample = EXAMPLE_WORDS
       .reduce((itIs, exampleWord) => {
         return itIs || nextPath.toLowerCase().includes(exampleWord);
       }, false);
 
+    // is the next path a directory or a file?
     const isDirectory = fs.statSync(path.normalize(path.join(dirPath, nextPath))).isDirectory();
+    // if it is a file, and not a javascript file, skip it
     if (!isDirectory && path.extname(nextPath) !== '.js') continue;
 
     if (isDirectory) {
+      // recursively register the path if it's a directory
+      //  this will create a virtual folder structure for this path
       const subDir = register(path.normalize(path.join(dirPath, nextPath)));
       try {
+        // check if that directory has an over-riding buttons configuration
+        //   if it does, add it to the virtual directory
         const subConfigStr = fs.readFileSync(path.normalize(path.join(EXERCISES_DIR, subDir.path, 'config.json')), 'utf-8');
         const subConfig = JSON.parse(subConfigStr);
         subDir.buttons = subConfig;
       } catch (err) { }
       if (subDir) {
+        // add the registered sub-directory to the current virtual directory
         dirs.push(subDir);
         if (isExample) subDir.isExample = isExample;
       };
     } else {
+      // create a file path object, and push it into the array of files in this directory
       const fileData = { path: '/' + nextPath };
       if (isExample) fileData.isExample = isExample;
       files.push(fileData);
     }
   };
 
+  // if the folder contains no .js files, ignore it
   const noExercises = files.length === 0 && dirs.length === 0;
   if (noExercises) {
     return null;
   };
 
+  // create the new virtual directory object
   const virDir = {
+    //  convert the absolute path to a relative path using unix file separators
     path: '/' + dirPath
       .split(path.sep).join('/')
       .split('/').pop(),
   };
+  // add the virtual files & sub-directories if they exist
   if (files.length > 0) virDir.files = files;
   if (dirs.length > 0) virDir.dirs = dirs;
 
+  // return the new virtual directory
   return virDir;
 };
 
 
 console.log('\n--- registering .js files in ' + removeDirname(EXERCISES_DIR) + ' ---\n');
+
+// register the /exercises directory
 const preRegistered = register(EXERCISES_DIR);
+// merge the repository config into the virtual directory
 const registered = Object.assign(preRegistered, {
   title: config.title,
   reviewPath: config.reviewPath,
   buttons: config.buttons
 });
+// set the date/time of last build
 registered.lastBuild = (new Date()).toJSON();
 
 console.log('\n--- writing /index.json ---\n');
+
+// write the file
 const stringifiedReg = JSON.stringify(registered, null, '  ');
 fs.writeFileSync(path.normalize(path.join('index.json')), stringifiedReg);
--- scripts/review.js
+++ scripts/review.js
@@ -1,68 +1,87 @@
+/*
+  this script uses ../index.json to run & evaluate each .js file in /exercises
+  after the review is finished it writes a README for each folder of exercises in /review
+*/
+
+// require dependencies (native node modules)
 const path = require('path');
 const fs = require('fs');
-const config = require("../config.json");
 
+// require evaluation & reporting scripts (non-native, stored in /lib)
 const reportify = require('./lib/reportify');
 const consoleCatcherFactory = require('./lib/console-catcher');
 const reviewify = require('./lib/reviewify');
 const reportThrownFactory = require('./lib/report-thrown');
 const evaluate = require('./lib/evaluate');
 
+// create a constant variable for the parent directory
 const PARENT_DIR = path.normalize(path.join(__dirname, '..'));
-const TITLE = config.title;
-
 
 console.log('\n--- loading ' + config.path + '/index.json ---\n');
 
 // index will be modified by side-effect
 const index = require(path.normalize(path.join(PARENT_DIR, 'index.json')));
 index.unsortedLogs = [];
-index.title = TITLE;
 
 console.log('\n--- generating empty reports ---\n');
 
+// add empty report objects to the virtual directory, one for each folder and file
 reportify.addEmptyReports(index);
 
 
 console.log('\n--- generating report map ---\n');
 
+// create another object that contains a shortcut to each empty report
+//  this is used for storing errors, logs and asserts
 const reportMap = reportify.generateReportMap(index);
 reportMap.unsortedLogs = index.unsortedLogs;
 
 console.log('\n--- setting up evaluation environment ---\n');
 
+// configure the function that will report any thrown exceptions
 const reportThrown = reportThrownFactory(PARENT_DIR, reportMap);
 
+// point to native console methods for debugging and logging in this script
 const nativeConsole = Object.assign({}, console);
+// over-write the global console object to intercept information from the exercises
 Object.assign(console, consoleCatcherFactory(PARENT_DIR, reportMap));
 
+// to catch & report any asynchronous errors that are not from promises
+//  (all synchronous errors are caught at evaluation)
 process.on('uncaughtException', err => {
   reportThrown(err, '', true);
 });
 
+// to catch & report unhandled promise rejections
 process.on('unhandledRejection', err => {
   reportThrown(err, '', true, true);
 });
 
-
+// do these things after all exercises have been evaluated and the event loop is cleared
 process.on('exit', (exitCode) => {
   try {
+    // go through all of the information captured during evaluation and summarize it
+    //  ie. look through each assertion/error and decide if a particular file is passed, failed or errored
     nativeConsole.log('\n--- interpreting reports ---\n');
     reportify.summarizeReports(index);
 
+    // render markdown for each file/folder based on the summaries created above
     nativeConsole.log('\n--- generating reviews\'s ---\n');
     reviewify.generateReviews(index);
 
+    // append any asynchronous exceptions that didn't have a callstack as an extra section in the main README
     index.review += reviewify.renderUnsorted(index.unsortedLogs);
 
     nativeConsole.log('\n--- writing reviews\'s ---\n');
 
-    index.reviewPath = config.reviewPath;
+    // write the new markdown files!
+    index.reviewPath = index.reviewPath;
     fs.rmdirSync(path.normalize(path.join(PARENT_DIR, index.reviewPath)), { recursive: true });
     fs.mkdirSync(path.normalize(path.join(PARENT_DIR, index.reviewPath)));
 
     reviewify.writeReviews(index, PARENT_DIR);
 
+    // done!
     nativeConsole.log(`exiting with code: ${exitCode}`);
 
   } catch (err) {
@@ -73,8 +92,11 @@ process.on('exit', (exitCode) => {
 
 nativeConsole.log('\n--- evaluating .js files ---\n');
 
+// add the date/time of most recent evaluation
 index.lastEvaluation = (new Date()).toJSON();
 
+// actually evaluate the exercises
+//  check out /lib/evaluate.js for more details
 evaluate(reportMap, PARENT_DIR, reportThrown);
 
 
