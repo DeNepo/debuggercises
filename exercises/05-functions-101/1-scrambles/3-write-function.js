a0bf3aca53820927999e0904d70539567b9ad7d6
Evan Cole <18554853+colevandersWands@users.noreply.github.com>
2020-06-22 16:59:13 +0200

    Update 3-write-function.js

--- exercises/05-functions-101/1-scrambles/3-write-function.js
+++ exercises/05-functions-101/1-scrambles/3-write-function.js
@@ -12,7 +12,7 @@ const _2_expect = "zxy";
 const _2_actual = scramble('x', 'y', 'z');
 console.assert(_2_actual === _2_expect, 'Test 2');
 
-const _3_expect = "zxy";
-const _3_actual = scramble('x', 'z', 'y');
+const _3_expect = "yzx";
+const _3_actual = scramble('z', 'x', 'y');
 console.assert(_3_actual === _3_expect, 'Test 3');
 
