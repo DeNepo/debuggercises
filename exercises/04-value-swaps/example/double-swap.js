496240edf0fceca2a090e4d37c0ab1eb033e7d86
colevandersWands <18554853+colevandersWands@users.noreply.github.com>
2020-06-23 08:00:57 +0200

    stepped in one file

--- exercises/04-value-swaps/example/double-swap.js
+++ exercises/04-value-swaps/example/double-swap.js
@@ -1,6 +1,6 @@
 'use strict';
 
-// this file shows the second line of code to write
+// this file shows the final line of code, with all tests passing
 
 // declare & assign variables
 let a = "y";
@@ -8,8 +8,13 @@ let b = "x";
 let temp = null;
 
 // swap the values stored by a and b ...
+
+// 1: store a's value for later
 temp = a;
+// 2: assign b's value to a
 a = b;
+// 3: assign the saved value to b
+b = temp;
 
 
 // assert expected values
