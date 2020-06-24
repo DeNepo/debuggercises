c2f528d4f8f625703e405039bf4ac9b95d6f4677
colevandersWands <18554853+colevandersWands@users.noreply.github.com>
2020-06-24 18:44:03 +0200

    better function names

--- exercises/08-functions-201/1-exercise.js
+++ exercises/08-functions-201/1-exercise.js
@@ -1,19 +1,19 @@
 'use strict';
 
 // write two different implementations with the same behavior
-// you can pass these using only ! and &&
+// you can pass these using only !, ||, and &&
 /*  hint: demorgan
   https://erikmhsiao.github.io/de-morgans-laws/
   https://www.freecodecamp.org/news/a-definitive-guide-to-conditional-logic-in-javascript-23fa234d2ca3/
   https://www.cross-browser.com/talk/demorgan.html
 */
 
-const notAndNotA = (x, y) => {
+const withAnd = (x, y) => {
   return _;
 };
 
 
-const notAndNotB = (x, y) => {
+const withOr = (x, y) => {
   return _;
 };
 
@@ -35,29 +35,29 @@ const _4_arg2 = 1;
 const _4_expect = 0;
 
 
-// testing notAndNotA
-const _1_actualA = notAndNotA(_1_arg1, _1_arg2);
+// testing withAnd
+const _1_actualA = withAnd(_1_arg1, _1_arg2);
 console.assert(_1_actualA === _1_expect, 'Test 1 A');
 
-const _2_actualA = notAndNotA(_2_arg1, _2_arg2);
+const _2_actualA = withAnd(_2_arg1, _2_arg2);
 console.assert(_2_actualA === _2_expect, 'Test 2 A');
 
-const _3_actualA = notAndNotA(_3_arg1, _3_arg2);
+const _3_actualA = withAnd(_3_arg1, _3_arg2);
 console.assert(_3_actualA === _3_expect, 'Test 3 A');
 
-const _4_actualA = notAndNotA(_4_arg1, _4_arg2);
+const _4_actualA = withAnd(_4_arg1, _4_arg2);
 console.assert(_4_actualA === _4_expect, 'Test 4 A');
 
 
-// testing notAndNotB
-const _1_actualB = notAndNotB(_1_arg1, _1_arg2);
+// testing withOr
+const _1_actualB = withOr(_1_arg1, _1_arg2);
 console.assert(_1_actualB === _1_expect, 'Test 1 B');
 
-const _2_actualB = notAndNotB(_2_arg1, _2_arg2);
+const _2_actualB = withOr(_2_arg1, _2_arg2);
 console.assert(_2_actualB === _2_expect, 'Test 2 B');
 
-const _3_actualB = notAndNotB(_3_arg1, _3_arg2);
+const _3_actualB = withOr(_3_arg1, _3_arg2);
 console.assert(_3_actualB === _3_expect, 'Test 3 B');
 
-const _4_actualB = notAndNotB(_4_arg1, _4_arg2);
+const _4_actualB = withOr(_4_arg1, _4_arg2);
 console.assert(_4_actualB === _4_expect, 'Test 4 B');
--- exercises/08-functions-201/2-exercise.js
+++ exercises/08-functions-201/2-exercise.js
@@ -1,18 +1,18 @@
 'use strict';
 
 // write two different implementations with the same behavior
-// you can pass these using only ! and ||
+// you can pass these using only !, && and ||
 /*  hint: demorgan
   https://erikmhsiao.github.io/de-morgans-laws/
   https://www.freecodecamp.org/news/a-definitive-guide-to-conditional-logic-in-javascript-23fa234d2ca3/
   https://www.cross-browser.com/talk/demorgan.html
 */
 
-const notOrNotA = (x, y) => {
+const withAnd = (x, y) => {
   return _;
 };
 
-const notOrNotB = (x, y) => {
+const withOr = (x, y) => {
   return _;
 };
 
@@ -34,29 +34,29 @@ const _4_arg2 = 1;
 const _4_expect = 0;
 
 
-// testing notOrNotA
-const _1_actualA = notOrNotA(_1_arg1, _1_arg2);
+// testing withAnd
+const _1_actualA = withAnd(_1_arg1, _1_arg2);
 console.assert(_1_actualA === _1_expect, 'Test 1 A');
 
-const _2_actualA = notOrNotA(_2_arg1, _2_arg2);
+const _2_actualA = withAnd(_2_arg1, _2_arg2);
 console.assert(_2_actualA === _2_expect, 'Test 2 A');
 
-const _3_actualA = notOrNotA(_3_arg1, _3_arg2);
+const _3_actualA = withAnd(_3_arg1, _3_arg2);
 console.assert(_3_actualA === _3_expect, 'Test 3 A');
 
-const _4_actualA = notOrNotA(_4_arg1, _4_arg2);
+const _4_actualA = withAnd(_4_arg1, _4_arg2);
 console.assert(_4_actualA === _4_expect, 'Test 4 A');
 
 
-// testing notOrNotB
-const _1_actualB = notOrNotB(_1_arg1, _1_arg2);
+// testing withOr
+const _1_actualB = withOr(_1_arg1, _1_arg2);
 console.assert(_1_actualB === _1_expect, 'Test 1 B');
 
-const _2_actualB = notOrNotB(_2_arg1, _2_arg2);
+const _2_actualB = withOr(_2_arg1, _2_arg2);
 console.assert(_2_actualB === _2_expect, 'Test 2 B');
 
-const _3_actualB = notOrNotB(_3_arg1, _3_arg2);
+const _3_actualB = withOr(_3_arg1, _3_arg2);
 console.assert(_3_actualB === _3_expect, 'Test 3 B');
 
-const _4_actualB = notOrNotB(_4_arg1, _4_arg2);
+const _4_actualB = withOr(_4_arg1, _4_arg2);
 console.assert(_4_actualB === _4_expect, 'Test 4 B');
